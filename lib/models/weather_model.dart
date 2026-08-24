2a2dd6873d224fa9f2540661ce88b1758dadd4a5
RokhayaB09 <rboye1309@gmail.com>
2026-08-24 21:10:21 +0000

    ajout de la direction du vent au modele meteo

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -5,13 +5,36 @@ class WeatherModel {
   final double latitude;
   final double longitude;
 
+  final double feelsLike;
+  final double tempMin;
+  final double tempMax;
+  final int humidity;
+  final double windSpeed;
+  final int windDirection;
+  final int pressure;
+  final int visibility;
+  final DateTime sunrise;
+  final DateTime sunset;
+  final double? rainVolume;
+  final double? snowVolume;
+
   WeatherModel({
     required this.city,
     required this.temperature,
     required this.description,
     required this.latitude,
     required this.longitude,
-
-});
-
+    required this.feelsLike,
+    required this.tempMin,
+    required this.tempMax,
+    required this.humidity,
+    required this.windSpeed,
+    required this.windDirection,
+    required this.pressure,
+    required this.visibility,
+    required this.sunrise,
+    required this.sunset,
+    this.rainVolume,
+    this.snowVolume,
+  });
 }
